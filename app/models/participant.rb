c3f8b595afc5466c5574286ca3be53752479f803
Poilon <poilon@gmail.com>
2015-01-15 21:33:14 +0100

    improved participant#edit

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -18,23 +18,37 @@ class Participant < ActiveRecord::Base
   end
 
   def global_rank
-    Participant.where(game_id: game.id).where('score > ?', score).count + 1
+    game_participants.where('score > ?', score).count + 1
   end
 
   def country_rank
-    Participant.where(game_id: game.id).where('country = ? and score > ?', country, score).count + 1
+    game_participants.where('country = ? and score > ?', country, score).count + 1
   end
 
   def state_rank
-    Participant.where(game_id: game.id).where('country = ? and state = ? and score > ?', country, state, score).count + 1
+    game_participants.where('country = ? and state = ? and score > ?', country, state, score).count + 1
   end
 
   def sub_state_rank
-    Participant.where(game_id: game.id).where('country = ? and sub_state = ? and state = ? and score > ?', country, sub_state, state, score).count + 1
+    game_participants.where('country = ? and sub_state = ? and state = ? and score > ?', country, sub_state, state, score).count + 1
   end
 
   def city_rank
-    Participant.where(game_id: game.id).where('country = ? and sub_state = ? and state = ? and city = ? and score > ?', country, sub_state, state, city, score).count + 1
+    game_participants.where('country = ? and sub_state = ? and state = ? and city = ? and score > ?', country, sub_state, state, city, score).count + 1
+  end
+
+  def next_target
+    if location
+      game_participants.where('score > ?', score).order('score asc').first
+    end
+  end
+
+  def training_partners
+    return city_buddies if city && city_rank > 1
+    return sub_state_buddies if sub_state && sub_state_rank > 1
+    return state_buddies if state && state_rank > 1
+    return country_buddies if country && country_rank > 1
+    return world_buddies
   end
 
   def self.clean
@@ -42,10 +56,61 @@ class Participant < ActiveRecord::Base
   end
 
   def merge_process
-    if participant = Participant.find_by_name(self.name)
+    if participant = game_participants.find_by_name(self.name)
       results.each { |r| r.update_attribute(:participant_id, participant.id) }
       self.reload.destroy
       participant.compute_score
     end
+  end
+
+  private
+
+  def city_buddies
+    city_players = game_participants.where('country = ? and sub_state = ? and state = ? and city = ?', country, sub_state, state, city).order('score desc')
+    better = city_players.where('score > ?', score).last(2)
+    tie = city_players.where('name <> ? and score = ?', name, score).sample(2)
+    worst = city_players.where('score < ?', score).first(2)
+
+    tie + better + worst
+  end
+
+  def sub_state_buddies
+    players = game_participants.where('country = ? and sub_state = ? and state = ?', country, sub_state, state).order('score desc')
+    better = players.where('score > ?', score).last(2)
+    tie = players.where('name <> ? and score = ?', name, score).sample(2)
+    worst = players.where('score < ?', score).first(2)
+
+    tie + better + worst
+  end
+  
+  def state_buddies
+    players = game_participants.where('country = ? and state = ?', country, state).order('score desc')
+    better = players.where('score > ?', score).last(2)
+    tie = players.where('name <> ? and score = ?', name, score).sample(2)
+    worst = players.where('score < ?', score).first(2)
+
+    tie + better + worst
+  end
+  
+  def country_buddies
+    players = game_participants.where('country = ?', country).order('score desc')
+    better = players.where('score > ?', score).last(2)
+    tie = players.where('name <> ? and score = ?', name, score).sample(2)
+    worst = players.where('score < ?', score).first(2)
+
+    tie + better + worst
+  end
+
+  def world_buddies
+    players = game_participants.order('score desc')
+    better = players.where('score > ?', score).last(2)
+    tie = players.where('name <> ? and score = ?', name, score).sample(2)
+    worst = players.where('score < ?', score).first(2)
+
+    tie + better + worst
+  end
+
+  def game_participants
+    Participant.where(game_id: game.id)
   end
 end
